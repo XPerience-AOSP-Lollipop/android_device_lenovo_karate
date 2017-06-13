3bd5aced8b3a3d614b8a2879ecc491826b988a99
klozz <xxx.reptar.rawrr.xxx@gmail.com>
2017-06-13 16:38:08 -0500

    karate:add missing telephony and black list ro.product.model we set it from init
    Signed-off-by: klozz <xxx.reptar.rawrr.xxx@gmail.com>

--- device.mk
+++ device.mk
@@ -17,6 +17,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 $(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk)
 $(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-dalvik-heap.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, vendor/XPe/config/common_full_phone.mk)
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
